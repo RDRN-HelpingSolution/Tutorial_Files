67c1938278f268b52b5f9dcf7c3b34d7ae479284
Roushan Agrawal <helpingrdrnsolution@gmail.com>
2024-09-19 18:26:41 +0530

    update

--- Admin/JavaScript/5c_Q_on_While_loop.js
+++ Admin/JavaScript/5c_Q_on_While_loop.js
@@ -7,26 +7,29 @@ console.log("while loop..............................................");
 console.log();
 
 // .......................................................................
-
 console.log("Q : Sum of Even Numbers");
 console.log();
-var a=0,b=10,sum=0,r=a;
-while (r%2==0&&r<=b) {
-     sum=sum+r;
-     r=r+2;
+var a = 0,
+  b = 10,
+  sum = 0,
+  r = a;
+while (r % 2 == 0 && r <= b) {
+  sum = sum + r;
+  r = r + 2;
 }
-console.log("Sum of Even Numbers between "+a+" to "+b+" is "+sum);  
+console.log("Sum of Even Numbers between " + a + " to " + b + " is " + sum);
 console.log();
 
 // .......................................................................
 
 console.log("Q : Factorial Calculation");
 console.log();
- var a=5,fac=1;
- while (a>=1) {
-       fac=fac*a;
-       a=a-1
- }
+var a = 5,
+  fac = 1;
+while (a >= 1) {
+  fac = fac * a;
+  a = a - 1;
+}
 console.log(fac);
 console.log();
 
@@ -34,11 +37,13 @@ console.log();
 
 console.log("Q : Table of any Number");
 console.log();
-var i=1 ,num=50,r;
-while (i<=10) {
-      r=num*i;
-      console.log(num+" * "+i+" = "+r);
-      i++;
+var i = 1,
+  num = 50,
+  r;
+while (i <= 10) {
+  r = num * i;
+  console.log(num + " * " + i + " = " + r);
+  i++;
 }
 console.log();
 
@@ -46,4 +51,3 @@ console.log();
 
 console.log("Q : Fibonacci numbers");
 console.log();
-
