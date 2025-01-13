9df11a66ab569b6c416dd25b83ad40b18a29411f
Roushan Agrawal <helpingrdrnsolution@gmail.com>
2025-01-13 12:51:44 +0530

    update

--- Coverage/JAVA/c1_hello.java
+++
@@ -1,7 +0,0 @@
-package Coverage.JAVA;
-
-public class c1_hello{
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
--- Coverage/JAVA/loops.java
+++
@@ -1,9 +0,0 @@
-package Coverage.JAVA;
-
-public class loops {
-    public static void main(String[] args) {
-       for(int i=40;i<=50;i++){
-        System.out.println("count is  "+i);
-       }
-    }
-}
