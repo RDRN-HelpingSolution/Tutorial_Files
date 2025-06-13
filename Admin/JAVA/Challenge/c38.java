161844511435af976544f71d6bfcbd637373550d
Roushan Agrawal <helpingrdrnsolution@gmail.com>
2025-06-13 19:57:51 +0530

    update

--- Admin/JAVA/Challenge/c38.java
+++ Admin/JAVA/Challenge/c38.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 public class c38 {
     public static int userInput(){
         System.out.println();
-        System.out.print("Provide a Number ");
+        System.out.print("Provide a Number :");
         Scanner s=new Scanner(System.in);
         int num=s.nextInt();
         s.close();
         return num;
     }
-    public static 
-    public static void main(String[] args) {
-        System.out.println();
-        int num=121;
+    public static int cal(int num){
         int x=1;
         int s=0;
         while (x>0) {
@@ -24,5 +21,21 @@ public class c38 {
             x=x/10;
             num=x;
         }
+        return s;
+    } 
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Welcome RDRN");
+           int x1=userInput();
+           int rf=cal(x1);
+        if (x1==rf) {
+            System.out.println("Provide Number "+x1+" is Palindrome Number");
+        }else{
+            System.out.println("Provide Number "+x1+" is Not Palindrome Number");
+        }
+        System.out.println();
+        
     }
-}
+       
+    }
+
