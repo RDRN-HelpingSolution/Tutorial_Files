c5b92b07260686c9a94c4262ea7db45b4fd97daa
Lenovo <bimiya2061@oremal.com>
2026-02-02 07:18:12 +0530

    Update

---
+++ Coverage/JAVA/PW/Recursion/Q12.java
@@ -0,0 +1,14 @@
+package Coverage.JAVA.PW.Recursion;
+
+// Print Zig-Zag ?
+//input           output
+//1               111
+//2               211121112
+//3               321112111232111211123
+//4               432111211123211121112343211121112321112111234
+
+public class Q12 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
