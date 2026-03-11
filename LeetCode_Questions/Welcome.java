c2455740e09c86c3e25b5eb1b0ed7cc5dcc28641
Lenovo <bimiya2061@oremal.com>
2026-03-11 15:27:35 +0530

    update

---
+++ LeetCode_Questions/Welcome.java
@@ -0,0 +1,9 @@
+package LeetCode_Questions;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to the LeetCode Scratchpad!\n");
+        System.out.println("Copy, solve, submit, and then delete problems here.\n");
+    }
+}
+
